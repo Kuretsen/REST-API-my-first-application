cb0d410f58b437d2fcf518c2dc4faaa856a2b805
Wojciech Nesteruk <wojciech.nesteruk@gmail.com>
2023-06-21 19:51:19 +0200

    23.4 Problem with email sending

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -3,20 +3,29 @@ package com.crud.tasks.service;
 import com.crud.tasks.domain.Mail;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class SimpleEmailService {
+    @Value("${spring.mail.username}")
+    private String userName;
+    @Value("${spring.mail.password}")
+    private String password;
 
     private final JavaMailSender javaMailSender;
 
     public void send(final Mail mail) {
         log.info("Starting email preparation...");
+        log.info("userName = " + userName);
+        log.info("password = " + password);
         try {
             SimpleMailMessage mailMessage = createMailMessage(mail);
             javaMailSender.send(mailMessage);
@@ -31,6 +40,10 @@ public class SimpleEmailService {
         mailMessage.setTo(mail.getMailTo());
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+//        Optional<String> mailCc = mail.getToCc();
+//        if (mailCc.isPresent()) {
+//            mailMessage.setCc(mailCc.get());
+//        }
         return mailMessage;
     }
 }
